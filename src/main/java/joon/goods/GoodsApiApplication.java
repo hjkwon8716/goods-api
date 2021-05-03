136c1017e5c87ef29b7a6fc0a2065d732de71844
hyukjoonkwon <hjkwon8716@naver.com>
2021-05-03 22:25:29 +0900

    add project 구조 commit  gradle, spring-boot, spring-data, spring-web, h2, lombok add
    modify .gitignore gradle 관련 내용 추가

---
+++ src/main/java/joon/goods/GoodsApiApplication.java
@@ -0,0 +1,13 @@
+package joon.goods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodsApiApplication.class, args);
+	}
+
+}
